bc63989c978d1be71c6951354b7c56f76123e91f
Frank Lam <franklam2008@gmail.com>
2022-07-18 00:27:22 -0400

    sql comment

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#     host="franklam2008.mysql.pythonanywhere-services.com",
+#     user="franklam2008",
+#     password="qQ_6RdzCfDkg@@_"
+# )
+
+# if(mydb):
+#     print("mydb", mydb)
+# else:
+#     print('mydb undefined')
+
 # mock students list
 
 STUDENTS = {
