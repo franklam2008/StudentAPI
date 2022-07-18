443fb993d8e084f45cbde222b4e0d18158fc424d
Frank Lam <franklam2008@gmail.com>
2022-07-18 00:52:53 -0400

    refactoring

---
+++ studentClass.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from studentsData import STUDENTS
+
+
+class Student(Resource):
+    def get(self, student_id):
+        if student_id not in STUDENTS:
+            return "Not found", 404
+        else:
+            return STUDENTS[student_id]
+
+    def put(self, student_id):
+        name = request.args.get('name')
+        age = request.args.get('age')
+        spec = request.args.get('spec')
+        if student_id not in STUDENTS:
+            return "Record not found", 404
+        else:
+            student = STUDENTS[student_id]
+            student["name"] = name if name is not None else student["name"]
+            student["age"] = age if age is not None else student["age"]
+            student["spec"] = spec if spec is not None else student["spec"]
+            return student, 200
+
+    def delete(self, student_id):
+        if student_id not in STUDENTS:
+            return "Not found", 404
+        else:
+            del STUDENTS[student_id]
+            return '', 204
---
+++ studentsListClass.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from studentsData import STUDENTS
+
+class StudentsList(Resource):
+    def get(self):
+        return STUDENTS
+
+    def post(self):
+        name = request.args.get('name')
+        age = request.args.get('age')
+        spec = request.args.get('spec')
+
+        student_id = int(max(STUDENTS.keys())) + 1
+        student_id = '%i' % student_id
+
+        if name and age and spec:
+            STUDENTS[student_id] = {
+                "name": name,
+                "age": age,
+                "spec": spec,
+            }
+            return STUDENTS[student_id], 201
+        else:
+            return "Args ERROR", 404
+
