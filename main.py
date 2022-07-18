e93de4615442947de2b06177752a2f71206c8c14
Frank Lam <franklam2008@gmail.com>
2022-07-18 01:12:10 -0400

    add jwt auth

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ from studentClass import Student
 from studentsData import STUDENTS
 from studentsListClass import StudentsList
 
+import jwt
+
 app = Flask(__name__)
 api = Api(app)
+
+# JWT authentication
+# =================================================
+payload = {"username": "Frank Lam", "role": "admin"}
+key = "SUPER_TEST_SECRET"
+encoded = jwt.encode(payload, key, algorithm="HS256")
+print('!!!encoded', encoded)
+test = jwt.decode(encoded, key, algorithms="HS256")
+print('!!!test payload', test)
+
 
 api.add_resource(StudentsList, '/students/')
 api.add_resource(Student, '/students/<student_id>')
@@ -14,8 +26,6 @@ api.add_resource(Student, '/students/<student_id>')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
 
 
 # import mysql.connector
